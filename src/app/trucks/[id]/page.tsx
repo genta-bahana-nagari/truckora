f5a87146e147f46777269bfcfed630f68c887ac1
gentabahananagari <gentapossenti@gmail.com>
2026-03-12 19:50:30 +0700

    feat: initial fleet page and not found fallback

--- src/app/trucks/[id]/page.tsx
+++ src/app/trucks/[id]/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+"use client";
 
-const TruckIDs = () => {
+import Link from "next/link";
+
+import { useParams } from "next/navigation";
+import { trucks } from "@/data/trucks";
+
+export default function TruckPage() {
+  const params = useParams();
+  const truck = trucks.find((t) => t.id === params?.id);
+
+  if (!truck)
+    return (
+      <section className="min-h-screen flex flex-col justify-center bg-white py-32">
+        <div className="mx-auto max-w-7xl px-4 lg:px-8">
+          <div className="text-center mb-16">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Truck Not Found
+            </h2>
+            <p className="mt-4 text-lg text-gray-600 max-w-2xl">
+              Oops! The truck you’re looking for doesn’t exist in our fleet.{" "}
+              <br />
+              Please return to our fleet page to explore other dependable trucks
+              built to move your goods safely, efficiently, and on time.
+            </p>
+            <Link
+              href="/trucks"
+              className="inline-block mt-6 rounded-lg bg-black px-6 py-3 text-white font-medium hover:bg-gray-800 transition-colors"
+            >
+              Back to Fleet
+            </Link>
+          </div>
+        </div>
+      </section>
+    );
+
   return (
-    <div>TruckIDs</div>
-  )
+    <div>
+      <h1>{truck.name}</h1>
+      <p>Brand: {truck.brand}</p>
+      <p>Series: {truck.series}</p>
+      <p>Price: ${truck.pricePerDay}/day</p>
+    </div>
+  );
 }
-
-export default TruckIDs
