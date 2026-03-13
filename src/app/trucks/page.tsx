065f743a2655ae5e46d2cfbf64edf8e079ee18c8
gentabahananagari <gentapossenti@gmail.com>
2026-03-13 10:56:18 +0700

    feat: minor revisions

--- src/app/trucks/page.tsx
+++ src/app/trucks/page.tsx
@@ -6,7 +6,6 @@ import Link from "next/link";
 import { trucks } from "@/data/trucks";
 
 export default function Trucks() {
-
   const featuredTrucks = trucks;
 
   return (
@@ -14,7 +13,7 @@ export default function Trucks() {
       id="featured"
       className="relative isolate overflow-hidden justify-center bg-white py-20 sm:py-26"
     >
-      <div className="mx-auto max-w-7xl px-4 lg:px-8">
+      <div className="mx-auto md:mx-16 max-w-screen px-4 lg:px-8">
         <div className="text-center mb-16">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Our Available Fleet
@@ -30,7 +29,7 @@ export default function Trucks() {
             <Link
               href={`trucks/${truck.id}`}
               key={truck.id}
-              className="mx-4 my-6 md:mx-0 md:my-0 group relative cursor-pointer rounded-2xl border border-gray-200 shadow-sm hover:shadow-lg hover:ring-2
+              className="mx-4 my-auto md:my-6 md:mx-0 md:my-0 group relative cursor-pointer rounded-2xl border border-gray-200 shadow-sm hover:shadow-lg hover:ring-2
                   hover:ring-black/5 transition-all duration-200"
             >
               <div className="aspect-square w-full overflow-hidden rounded-t-2xl bg-gray-200">
@@ -39,19 +38,19 @@ export default function Trucks() {
                   alt={truck.name}
                   width={400}
                   height={200}
-                  className="h-full w-full object-cover object-center group-hover:opacity-90 transition-opacity duration-200"
+                  className="group-hover:opacity-75 h-full w-full object-cover object-center group-hover:opacity-90 transition-opacity duration-200"
                 />
               </div>
 
-              <div className="mt-4 px-2 pb-4">
-                <h3 className="text-md font-medium text-gray-900">
-                  {truck.name}
-                </h3>
-                <p className="text-sm text-gray-500">
-                  {truck.brand} - {truck.series}
-                </p>
-                <p className="text-sm font-semibold text-gray-500 my-1">
-                  Rental rate: ${truck.pricePerDay}/day
+              <div className="mt-4 flex justify-between px-4 mb-4">
+                <div>
+                  <h3 className="text-sm text-gray-700">{truck.name}</h3>
+                  <p className="mt-1 text-sm text-gray-500">
+                    {truck.brand} {truck.series}
+                  </p>
+                </div>
+                <p className="text-sm font-semibold text-gray-900">
+                  ${truck.pricePerDay} / day
                 </p>
               </div>
             </Link>
