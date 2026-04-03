983b9300e80f87931695894d3e795ef21f24a556
gentabahananagari <gentapossenti@gmail.com>
2026-04-03 21:52:51 +0700

    feat: redirect from vercel domain to custom domain

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,21 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "truckora.vercel.app",
+          },
+        ],
+        destination: "https://www.truckora.gentabahana.web.id/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
